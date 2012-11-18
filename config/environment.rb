175df13ff6f8c0eb9609568b4af5a58a4a4d4b49
deepak vokaliga <deepak.vokaliga@gmail.com>
2012-11-18 03:29:06 -0500

    envs

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+ENV['SINGLY_ID']='c5c92eceb53d5f7561be5fc6aa1595ed'
+ENV['SINGLY_SECRET']='c52a4124dee094c0c9eced1bb7b8d30b'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
